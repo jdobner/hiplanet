a204a45bd60440c1589d0391fb1cd2c5749ada78
Jerry Dobner <jdobner@gmail.com>
2017-03-08 22:19:00 -0500

    initial

---
+++ trading-engine/src/com/liquidx/te/Main.java
@@ -0,0 +1,18 @@
+package com.liquidx.te;
+
+/**
+ * Created by jdobner on 3/7/2017.
+ */
+
+public class Main {
+
+  private static final String TAG="Main";
+
+  public static void main(String[] args) {
+    new Main().start();
+  }
+
+  private void start() {
+
+  }
+}
